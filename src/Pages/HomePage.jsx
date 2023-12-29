539c6ca3b74a2efa6d5aaf5af47816a116f837de
Akash Kumar Singh <itsakash18.06@gmail.com>
2023-12-29 10:04:34 +0530

    Main section created

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -2,10 +2,37 @@ import React from 'react'
 
 import HomeLayout from '../Layouts/HomeLayout'
 const HomePage = () => {
+
     return (
         <HomeLayout>
-            <div className='text-white min-h-[90vh]'>
-            </div>
+            <main className='text-white min-h-[90vh]'>
+                <div></div>
+                <div>
+                    <div className="w-64 carousel rounded-box" >
+                        <div className="carousel-item w-full">
+                            <img src="https://daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg" className="w-full" alt="Tailwind CSS Carousel component" />
+                        </div>
+                        <div className="carousel-item w-full">
+                            <img src="https://daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg" className="w-full" alt="Tailwind CSS Carousel component" />
+                        </div>
+                        <div className="carousel-item w-full">
+                            <img src="https://daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.jpg" className="w-full" alt="Tailwind CSS Carousel component" />
+                        </div>
+                        <div className="carousel-item w-full">
+                            <img src="https://daisyui.com/images/stock/photo-1494253109108-2e30c049369b.jpg" className="w-full" alt="Tailwind CSS Carousel component" />
+                        </div>
+                        <div className="carousel-item w-full">
+                            <img src="https://daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg" className="w-full" alt="Tailwind CSS Carousel component" />
+                        </div>
+                        <div className="carousel-item w-full">
+                            <img src="https://daisyui.com/images/stock/photo-1559181567-c3190ca9959b.jpg" className="w-full" alt="Tailwind CSS Carousel component" />
+                        </div>
+                        <div className="carousel-item w-full">
+                            <img src="https://daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg" className="w-full" alt="Tailwind CSS Carousel component" />
+                        </div>
+                    </div>
+                </div>
+            </main>
         </HomeLayout>
     )
 }
