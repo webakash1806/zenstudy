2ad17360f19cceca8af0f3e0c1dd99487cea6936
Akash Kumar Singh <itsakash18.06@gmail.com>
2023-12-29 15:35:53 +0530

    Home Page created

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,12 +1,30 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import HomeLayout from '../Layouts/HomeLayout'
 const HomePage = () => {
 
     return (
         <HomeLayout>
-            <main className='text-white min-h-[90vh]'>
-                <div></div>
+            <main className='text-white min-h-[90vh] py-8 flex flex-col-reverse items-center justify-center'>
+                <div className='p-3 px-5 text-center'>
+                    <h1 className='text-[2.2rem] font-semibold flex flex-col'>
+                        Find Out Best
+                        <span className='pl-2 text-yellow-400'>
+                            Online Courses
+                        </span>
+                    </h1>
+                    <p className='font-[330] text-[0.97rem] py-3 tracking-[0.3px]'>
+                        Explore diverse coding courses, master programming languages, and build real-world projects with expert-led online tutorials for comprehensive skill development.
+                    </p>
+                    <div>
+                        <Link to='/courses'>
+                            <button className='bg-yellow-600 p-2 px-8 font-[500] tracking-wide rounded-md text-[1.05rem] hover:bg-yellow-700'>
+                                Explore Courses
+                            </button>
+                        </Link>
+                    </div>
+                </div>
                 <div>
                     <div className="w-64 carousel rounded-box" >
                         <div className="carousel-item w-full">
