b5d397647ad1e126da1e6b3ddbecd32ea6a7b066
Akash Kumar Singh <itsakash18.06@gmail.com>
2024-01-01 15:22:17 +0530

    404 error page responsive added

--- src/Pages/PageNotFound.jsx
+++ src/Pages/PageNotFound.jsx
@@ -6,10 +6,12 @@ import Error404 from '../assets/Error404.png'
 const PageNotFound = () => {
     const navigate = useNavigate()
     return (
-        <div className='bg-[#eaffff] w-screen h-screen flex flex-col items-center justify-center'>
-            <p className='text-[#000] font-bold text-[1.5rem] text-center mx-8'>Looks Like you&#39;re lost the address!</p>
-            <img className='w-[90%]' src={Error404} alt="404 Page not found Image!" />
-            <button onClick={() => navigate(-1)} className='bg-[#FFB827] text-black p-[5px] px-4 font-semibold rounded-md animate-bounce shadow-sm hover:bg-black hover:text-[#FFB827]'>Go Back!</button>
+        <div className='bg-[#eaffff] w-screen md:flex-row-reverse h-screen flex flex-col items-center justify-center'>
+            <img className='w-[19rem] sm:w-[22rem] lg:w-[26rem]' src={Error404} alt="404 Page not found Image!" />
+            <div className='flex flex-col items-center justify-center'>
+                <p className='text-[#000] font-bold text-[1.5rem] text-center mx-8'>Looks Like you&#39;re lost the address!</p>
+                <button onClick={() => navigate(-1)} className='mt-4 bg-[#FFB827] text-black p-[5px] px-4 font-semibold rounded-md animate-bounce shadow-sm hover:bg-black hover:text-[#FFB827]'>Go Back!</button>
+            </div>
         </div>
     )
 }
